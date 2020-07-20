d21ad1969ad8c784b55ca995d53536036ae74749
Mariana <marigonzalez96.mg@gmail.com>
2020-07-20 20:04:02 -0300

    Actualiza index

--- Js/main.js
+++ Js/main.js
@@ -1,2 +1,2 @@
-var FECHA_NACIMIENTO = new Date (1996,5,25)
-var edad = document.getElementById ("mi-edad")
+var FECHA_NACIMIENTO = new Date(1996,5,25)
+var edad = document.getElementById("mi-edad")
